cd0ac3e90d8186e909cab2e51f3e3b65c2255975
unknown <ahmedalif102@gmail.com>
2025-07-10 17:23:36 +0600

    react router setup & layout confirmed

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,6 @@ function App() {
 
   return (
     <>
-      <h1 className='text-3xl text-red-500'>
-        Bistro Boss
-      </h1>
     </>
   )
 }
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-6xl text-red-700">Home</h1>
+    </div>
+  );
+};
+
+export default Home;
